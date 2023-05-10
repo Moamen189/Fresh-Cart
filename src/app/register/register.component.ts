1c6aa631f5480cdfc3aabbb63a2cd4570f09d950
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-05-11 00:22:43 +0300

    RegisterValidation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import { FormGroup , FormControl } from '@angular/forms';
+import { FormGroup , FormControl , Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +10,17 @@ import { FormGroup , FormControl } from '@angular/forms';
  export class RegisterComponent  {
 
   registerForm:FormGroup = new FormGroup ({
-    name: new FormControl(null),
-    email: new FormControl(null),
-    password: new FormControl(null),
-    rePassword: new FormControl(null),
-    phone: new FormControl(null),
+    name: new FormControl(null , [Validators.required , Validators.minLength(3) , Validators.maxLength(15)]),
+    email: new FormControl(null , [Validators.required , Validators.email]),
+    password: new FormControl(null , [Validators.required , Validators.pattern(/^[A-Z]/)]),
+    rePassword: new FormControl(null , [Validators.required , Validators.pattern(/^[A-Z]/)]),
+    phone: new FormControl(null , [Validators.required])
 
   })
 
   handlelRegister(registerForm:FormGroup){
 
-    console.log(registerForm)
+    console.log(registerForm.value)
 
   }
 
