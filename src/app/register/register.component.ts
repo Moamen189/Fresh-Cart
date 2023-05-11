6c4a9772d9b1e39c0d7cd200252057e5e099858c
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-05-11 20:22:35 +0300

    UpdateRegister

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,6 +13,7 @@ import { Router } from '@angular/router';
 })
  export class RegisterComponent  {
   constructor(private _AuthService:AuthService , private _Router:Router) { }
+  isLoading:boolean = false;
   registerForm:FormGroup = new FormGroup ({
     name: new FormControl(null , [Validators.required , Validators.minLength(3) , Validators.maxLength(15)]),
     email: new FormControl(null , [Validators.required , Validators.email]),
@@ -23,18 +24,20 @@ import { Router } from '@angular/router';
   })
 
   handlelRegister(registerForm:FormGroup){
-
+    this.isLoading = true;
     if(registerForm.valid){
       this._AuthService.regiser(registerForm.value).subscribe({
 
         next: (response) => {
 
 
+          this.isLoading = false;
             this._Router.navigate(['/login'])
 
 
         },
         error: (err) => {
+          this.isLoading = false;
           this._Router.navigate(['/login'])
         }
       })
