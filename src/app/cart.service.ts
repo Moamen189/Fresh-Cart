3a42db822c9d7aed16bff68e83ab8f66dfe6667e
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-05-23 16:22:32 +0300

    UpdateCartService

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
 
 
 @Injectable({
@@ -6,5 +8,16 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+  headers:any =
+  {
+    token:localStorage.getItem('userToken')
+  }
+
+  addToCart(productId:string):Observable<any>
+{
+  return this._HttpClient.post(`https://route-ecommerce.onrender.com/api/v1/cart` , {productId:productId} ,{headers:this.headers} )
 }
+
+}
+
