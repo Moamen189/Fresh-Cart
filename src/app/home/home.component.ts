894cd595a64cc83d03d6f85cf6ebd90ff3cda504
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-05-23 16:40:08 +0300

    AddToCart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,22 @@ import { ProductsService } from '../products.service';
 export class HomeComponent implements OnInit {
   products:any[] = []
   searchTerm:string=''
-  constructor(private _ProductsService:ProductsService) { }
+  constructor(private _ProductsService:ProductsService , private _CartService:CartService) { }
+
+  addTocart(productId:string){
+    this._CartService.addToCart(productId).subscribe({
+      next:(res) => console.log(res) ,
+      error:(err) =>   console.log(err)
+    })
+
+  }
 
   ngOnInit(): void {
     this._ProductsService.getProducts().subscribe({
       next: (response) => this.products = response.data
     })
+
+
   }
 
 }
